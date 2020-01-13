44c3d49ec90615ae52c260ada045c81dcce8d056
Jeff Lett <jeffctown@gmail.com>
2020-01-13 17:47:57 -0500

    Complications looking okay.  Need cleanup on the assets and to make them dynamic.

--- Watch Extension/ComplicationController.swift
+++ Watch Extension/ComplicationController.swift
@@ -52,14 +52,12 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         handler(template(for: complication.family))
     }
     
+    var simpleTinyTitleProvider: CLKSimpleTextProvider {
+        CLKSimpleTextProvider(text: "Xcode 11.3", shortText: "11.3")
+    }
     
-    //beta #
-    //gm seed #
-    //gm
-    //GM1
-    //GM
-    var simpleTextProvider: CLKSimpleTextProvider {
-        CLKSimpleTextProvider(text: "Xcode: 11.3 GM Seed 1", shortText: "11.3 GM1")
+    var simpleTinyBodyProvider: CLKSimpleTextProvider {
+        CLKSimpleTextProvider(text: "GM Seed 1", shortText: "GM 1")
     }
     
     var simpleAppNameProvider: CLKSimpleTextProvider {
@@ -78,18 +76,27 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
         CLKFullColorImageProvider(fullColorImage: UIImage(named: "Xcode")!)
     }
     
-      
+    var logoSmallImageProvider: CLKFullColorImageProvider {
+        CLKFullColorImageProvider(fullColorImage: UIImage(named:"Xcode-Small")!)
+    }
+    
+    var logoFlatImageProvider: CLKImageProvider {
+        CLKImageProvider(onePieceImage: UIImage(named: "Xcode-Flat")!)
+    }
+    
+    var logoCircularImageProvider: CLKFullColorImageProvider {
+        CLKFullColorImageProvider(fullColorImage: UIImage(named: "Xcode-Circular")!)
+    }
+    
     func template(for family: CLKComplicationFamily) -> CLKComplicationTemplate {
         switch family {
-        case .circularSmall: //good
-            let template = CLKComplicationTemplateCircularSmallStackText()
-            template.line1TextProvider = simpleAppNameProvider
-            template.line2TextProvider = simpleTextProvider
+        case .circularSmall:
+            let template = CLKComplicationTemplateCircularSmallSimpleImage()
+            template.imageProvider = logoFlatImageProvider
             return template
         case .extraLarge:
-            let template = CLKComplicationTemplateExtraLargeStackText()
-            template.line1TextProvider = simpleAppNameProvider
-            template.line2TextProvider = simpleTextProvider
+            let template = CLKComplicationTemplateExtraLargeSimpleImage()
+            template.imageProvider = logoFlatImageProvider
             return template
         case .graphicBezel:
             let template = CLKComplicationTemplateGraphicBezelCircularText()
@@ -98,43 +105,49 @@ class ComplicationController: NSObject, CLKComplicationDataSource {
             return template
         case .graphicCircular:
             let template = CLKComplicationTemplateGraphicCircularImage()
-            template.imageProvider = logoImageProvider
+            template.imageProvider = logoCircularImageProvider
             return template
         case .graphicCorner:
             let template = CLKComplicationTemplateGraphicCornerCircularImage()
-            template.imageProvider = logoImageProvider
+            template.imageProvider = logoCircularImageProvider
             return template
         case .graphicRectangular:
             let template = CLKComplicationTemplateGraphicRectangularStandardBody()
+            template.headerImageProvider = logoSmallImageProvider
             template.headerTextProvider = simpleAppNameProvider
-            template.body1TextProvider = simpleTextProvider
+            template.body1TextProvider = simpleSingleTextProvider
             template.body2TextProvider = simpleReleaseDateProvider
             return template
         case .modularLarge:
             let template = CLKComplicationTemplateModularLargeStandardBody()
+            template.headerImageProvider = logoFlatImageProvider
             template.headerTextProvider = simpleAppNameProvider
-            template.body1TextProvider = simpleTextProvider
+            template.body1TextProvider = simpleSingleTextProvider
             template.body2TextProvider = simpleReleaseDateProvider
             return template
         case .modularSmall:
-            let template = CLKComplicationTemplateModularSmallStackText()
-            template.line1TextProvider = simpleAppNameProvider
-            template.line2TextProvider = simpleTextProvider
+            let template = CLKComplicationTemplateModularSmallSimpleImage()
+            template.imageProvider = logoFlatImageProvider
             return template
         case .utilitarianLarge:
             let template = CLKComplicationTemplateUtilitarianLargeFlat()
+            template.imageProvider = logoFlatImageProvider
             template.textProvider = simpleSingleTextProvider
             return template
         case .utilitarianSmall:
-            let template = CLKComplicationTemplateUtilitarianSmallRingText()
-            template.textProvider = simpleTextProvider
+            let template = CLKComplicationTemplateUtilitarianSmallSquare()
+            template.imageProvider = logoFlatImageProvider
             return template
         case .utilitarianSmallFlat:
             let template = CLKComplicationTemplateUtilitarianSmallFlat()
-            template.textProvider = simpleTextProvider
+            template.imageProvider = logoFlatImageProvider
+            template.textProvider = simpleTinyBodyProvider
             return template
-        default:
-            fatalError("WTH")
+        @unknown default:
+            assertionFailure("WTH")
+            let template = CLKComplicationTemplateUtilitarianSmallFlat()
+            template.textProvider = simpleSingleTextProvider
+            return template
         }
     }
     
